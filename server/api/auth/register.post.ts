c9f846886cab7875e91a111328fff7dbec49cff7
marcusslv <marcusjapaslv@hotmail.com>
2025-07-01 23:46:13 -0300

    :construction: Store para registar usuários

---
+++ server/api/auth/register.post.ts
@@ -0,0 +1,40 @@
+export default defineEventHandler(async (event): Promise <IResponse> => {
+  try {
+    const url = "http://localhost:8000/api/register";
+    const body = await readBody(event);
+
+    const response = await $fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body,
+    });
+
+    return  {
+      status: "success",
+      statusCode: 200,
+      message: "Cadastro realizado com sucesso",
+      data: response
+    } as IResponse;
+  } catch (err) {
+    const error = err as IError;
+
+    if (error?.statusCode === 422) {
+      return {
+        status: "error",
+        statusCode: error.statusCode,
+        message: error.message || "Dados inválidos",
+        data: error.data,
+      } as IResponse;
+    }
+
+    return {
+      status: "error",
+      statusCode: error?.statusCode || 500,
+      message: error.message || "Erro ao realizar cadastro",
+      data: error,
+    } as IResponse;
+  }
+});
