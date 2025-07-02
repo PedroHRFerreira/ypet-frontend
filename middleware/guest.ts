3f43c455dc14a8375b193c83ac90b1f53f4ded6c
marcusslv <marcusjapaslv@hotmail.com>
2025-07-01 22:38:20 -0300

    :sparkles: Adicionando store de autenticação.

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,7 +1,7 @@
-import { useAuthStore } from "~/stores/useAuthStore";
+import { useAuthLoginStore } from "~/stores/auth/useAuthLoginStore";
 
 export default defineNuxtRouteMiddleware(() => {
-	const auth = useAuthStore();
+	const auth = useAuthLoginStore();
 
 	if (auth.isLoggedIn === true) {
 		return navigateTo("/", { replace: true });
--- types/global.d.ts
+++ types/global.d.ts
@@ -5,5 +5,19 @@ declare global {
 		value?: number;
 		state?: "default" | "activated" | "disabled";
 	}
+
+  interface IAccessToken {
+    access_token: string;
+    expires_in: number;
+    token_type: string;
+    user: IUser;
+  }
+
+  interface IResponse {
+    status: "success" | "error";
+    statusCode: number;
+    message: string;
+    data: any;
+  }
 }
-export { IOption };
+export { IOption, IResponse, IAccessToken };
