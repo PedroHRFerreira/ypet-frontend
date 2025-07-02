6fa3195f09647c49358dfc7c45361a4a5ee0fc85
marcusslv <marcusjapaslv@hotmail.com>
2025-07-01 23:12:23 -0300

    :construction: Permitindo o logout via store

---
+++ server/api/auth/logoutAll.post.ts
@@ -0,0 +1,27 @@
+export default defineEventHandler(async (event): Promise <IResponse> => {
+  try {
+    const url = "http://localhost:8000/api/logout-all";
+
+    const response = await $fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `${getCookie(event, 'auth._token.laravelSanctum')}`,
+      },
+    });
+
+    return  {
+      status: "success",
+      statusCode: 200,
+      message: "Logout realizado com sucesso",
+    } as IResponse;
+  } catch (err) {
+    return {
+      status: "error",
+      statusCode: err?.statusCode || 500,
+      message: "Erro ao realizar logout",
+      data: err,
+    } as IResponse;
+  }
+});
