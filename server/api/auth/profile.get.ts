278c5376b537fbb9a2818180171cd4093e49568e
marcusslv <marcusjapaslv@hotmail.com>
2025-07-01 23:55:17 -0300

    :construction: Adicionando store para o profile

---
+++ server/api/auth/profile.get.ts
@@ -0,0 +1,21 @@
+export default defineEventHandler(async (event): Promise <IResponse> => {
+    const url = "http://localhost:8000/api/profile";
+    const body = await readBody(event);
+
+    const response = await $fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `${getCookie(event, 'auth._token.laravelSanctum')}`,
+      },
+      body,
+    });
+
+    return  {
+      status: "success",
+      statusCode: 200,
+      message: "Perfil carregado com sucesso",
+      data: response
+    } as IResponse;
+});
