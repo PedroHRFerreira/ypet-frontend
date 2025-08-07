35145cc8b0a69da8f5f9a0ed874621f560bcfc2c
marcusslv <marcusjapaslv@hotmail.com>
2025-08-07 07:10:24 -0300

    :art: Formatando o codigo

--- components/atoms/BaseRadio/index.vue
+++ components/atoms/BaseRadio/index.vue
@@ -8,38 +8,37 @@ export default defineComponent({
 			type: String,
 			required: true,
 		},
-    option: {
-      type: Object as PropType<IOption>,
-      required: false,
-      default: () => ({ id: '', text: 'Lorem Ipsum', state: 'default' }),
-    }
+		option: {
+			type: Object as PropType<IOption>,
+			required: false,
+			default: () => ({ id: "", text: "Lorem Ipsum", state: "default" }),
+		},
 	},
 	emits: ["onChange"],
 	setup(props, { emit }) {
+		const handleChange = (event: Event) => {
+			const target = event.target as HTMLInputElement;
+			emit("onChange", target.value);
+		};
 
-    const handleChange = (event: Event) => {
-      const target = event.target as HTMLInputElement;
-      emit('onChange', target.value);
-    };
-
-    const state = computed(() => {
-      return props.option?.state || 'default';
-    });
-    const value = computed(() => {
-      return props.option?.id || '';
-    });
-    const label = computed(() => {
-      return props.option?.text || 'Lorem Ipsum';
-    })
-    const isChecked = computed(() => {
-      return state.value === 'activated';
-    });
+		const state = computed(() => {
+			return props.option?.state || "default";
+		});
+		const value = computed(() => {
+			return props.option?.id || "";
+		});
+		const label = computed(() => {
+			return props.option?.text || "Lorem Ipsum";
+		});
+		const isChecked = computed(() => {
+			return state.value === "activated";
+		});
 
 		return {
-      value,
-      label,
-      isChecked,
-      handleChange
+			value,
+			label,
+			isChecked,
+			handleChange,
 		};
 	},
 });
@@ -51,10 +50,10 @@ export default defineComponent({
 			type="radio"
 			class="radio-input"
 			:value="value"
-      :checked="isChecked"
+			:checked="isChecked"
 			:name="name"
 			@input="handleChange"
-      @change="handleChange"
+			@change="handleChange"
 		/>
 		<span class="custom-radio"></span>
 		<span class="radio-label">{{ label }}</span>
