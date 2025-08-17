b3dd36588ff1a932455718a41682c6f4f09afa3f
marcusslv <marcusjapaslv@hotmail.com>
2025-08-17 14:55:50 -0300

    :construction: Criando submenu para logout

--- components/templates/Home.vue
+++ components/templates/Home.vue
@@ -3,24 +3,12 @@ import { defineComponent } from "vue";
 import { useAuthLoginStore } from "~/stores/auth/useAuthLoginStore";
 export default defineComponent({
 	name: "TemplatesHome",
-	setup() {
-		const useAuthLogin = useAuthLoginStore();
-
-		return {
-			useAuthLogin,
-		};
-	},
 });
 </script>
 
 <template>
 	<div class="section">
-		<h1>Hello World</h1>
-		<MoleculesButtonsCommon
-			type="primary"
-			text="Logout"
-			@onclick="useAuthLogin.logout"
-		/>
+		<h1>Home</h1>
 	</div>
 </template>
 <style scoped lang="scss">
