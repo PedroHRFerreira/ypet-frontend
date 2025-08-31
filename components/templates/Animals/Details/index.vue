ad44ac0b2291db57327d89719d370fc6563d2311
marcusslv <marcusjapaslv@hotmail.com>
2025-08-31 08:45:30 -0300

    Padronizando templates do contexto animal

--- components/templates/Animals/Details/index.vue
+++ components/templates/Animals/Details/index.vue
@@ -3,10 +3,115 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "TemplatesAnimalsDetails",
+	setup() {
+		const header = computed(() => {
+			return {
+				title: "Detalhes do animal",
+				subtitle: "Visualize e gerencie os detalhes do animal",
+				buttons: [
+					{
+						text: "Voltar",
+						type: "outline",
+						icon: "arrow-left",
+						iconLeft: true,
+						nameIconLeft: "arrow-left",
+						iconRight: false,
+						nameIconRight: "",
+						size: "small",
+						width: "auto",
+						action: () => {
+							const router = useRouter();
+							router.back();
+						},
+					},
+					{
+						text: "Editar",
+						type: "primary",
+						icon: "edit",
+						iconLeft: true,
+						nameIconLeft: "edit",
+						iconRight: false,
+						nameIconRight: "",
+						size: "small",
+						width: "auto",
+						action: () => {
+							const router = useRouter();
+							const animalId = useRoute().params.id;
+
+							router.push({ name: "animals-edit", params: { id: animalId } });
+						},
+					},
+				],
+			};
+		});
+
+		const emptyState = computed(() => {
+			return {
+				isEmpty: true,
+				isIcon: true,
+				title: "",
+				description: "",
+			};
+		});
+		return {
+			header,
+			emptyState,
+		};
+	},
 });
 </script>
 
-<template>Detalhes do animal</template>
+<template>
+	<div class="wrapper-template">
+		<div class="content">
+			<header class="header">
+				<div class="header-content">
+					<AtomsTypography
+						type="title-h7"
+						class="header-content__title"
+						:text="header.title"
+						weight="medium"
+						color="var(--brand-color-dark-blue-900)"
+					/>
+					<AtomsTypography
+						type="text-p4"
+						class="header-content__subtitle"
+						:text="header.subtitle"
+						weight="medium"
+						color="var(--brand-color-dark-blue-300)"
+					/>
+				</div>
+				<div class="header-actions">
+					<MoleculesButtonsCommon
+						v-for="button in header.buttons"
+						:key="button.text"
+						:type="button.type"
+						:text="button.text"
+						:icon-left="button.iconLeft"
+						:icon-right="button.iconRight"
+						:name-icon-left="button.nameIconLeft"
+						:name-icon-right="button.nameIconRight"
+						:size="button.size"
+						:width="button.width"
+						@onclick="button.action"
+					/>
+				</div>
+			</header>
+			<main class="main">
+				<div v-if="emptyState.isEmpty" class="main-empty">
+					<MoleculesEmptyState
+						:is-icon="emptyState.isIcon"
+						:title="emptyState.title"
+						:description="emptyState.description"
+					/>
+				</div>
+				<div v-else class="main-content">
+					<h2>Organismos de lista</h2>
+				</div>
+			</main>
+		</div>
+	</div>
+</template>
 
 <style scoped lang="scss">
 @use "style.module";
