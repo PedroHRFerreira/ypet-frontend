7d46bf7709614452d845a7489494856461939a85
marcusslv <marcusjapaslv@hotmail.com>
2025-09-01 08:00:23 -0300

    Ajustando Copy do login

--- layouts/access/access.vue
+++ layouts/access/access.vue
@@ -1,19 +1,55 @@
 <script lang="ts">
 export default defineComponent({
 	name: "LayoutAccess",
+	setup() {
+		const itemCopy = [
+			"Gerencie cadastros e permissões",
+			"Monitore denúncias e solicitações",
+			"Gere relatórios e estatísticas",
+		];
+		return {
+			itemCopy,
+		};
+	},
 });
 </script>
 <template>
 	<section class="access">
+		<AtomsIcon
+			class="access__logo-top"
+			name="logo-white"
+			width="100px"
+			height="124px"
+		/>
 		<div class="access__container">
 			<div class="access__copy">
 				<AtomsIcon name="logo-white" width="100px" height="124px" />
 				<AtomsTypography
-					type="title-h1"
+					type="title-h4"
 					weight="regular"
 					color="var(--white)"
-					text="Login to enjoy the huge ton of exclusive features"
+					text="Acesse o Painel para usar todos os recursos exclusivos"
 				/>
+				<div class="access__copy--list">
+					<div
+						v-for="(item, key) in itemCopy"
+						:key="key"
+						class="access__copy--list__item"
+					>
+						<AtomsIcon
+							name="check"
+							width="24px"
+							height="23px"
+							color="var(--white)"
+						/>
+						<AtomsTypography
+							type="text-p2"
+							weight="regular"
+							color="var(--white)"
+							:text="item"
+						/>
+					</div>
+				</div>
 			</div>
 			<div class="access__slot">
 				<slot />
@@ -22,5 +58,5 @@ export default defineComponent({
 	</section>
 </template>
 <style scoped lang="scss">
-@use "styles.module.scss";
+@use "styles.module";
 </style>
