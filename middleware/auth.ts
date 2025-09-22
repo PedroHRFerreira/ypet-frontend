27b24874cc577775ac47ffd2e0ca6203e6099ae8
herokk1 <daniel.ferreira2675@gmail.com>
2025-09-22 15:55:15 -0300

    fix: :fire: Tweaks para burlar Login

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,6 +4,6 @@ export default defineNuxtRouteMiddleware(() => {
 	const auth = useAuthLoginStore();
 
 	if (!auth.isLoggedIn()) {
-		return navigateTo("/login", { replace: true });
+		console.log("TWEAK PARA ACESSAR DASHBOARD SEM LOGIN FEITO");
 	}
 });
