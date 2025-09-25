f938449e386e4f4c0e0e83fd8d2583f3da723448
marcusslv <marcusjapaslv@hotmail.com>
2025-09-25 08:56:38 -0300

    Melhorias no organismos de criação

--- components/templates/CastraMobile/ClinicEvent/Edit/index.vue
+++ components/templates/CastraMobile/ClinicEvent/Edit/index.vue
@@ -2,7 +2,7 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	name: "TemplatesCastraMobileClinicEvent",
+	name: "TemplatesCastraMobileClinicEventEdit",
 	setup() {
 		const header = computed(() => {
 			return {
