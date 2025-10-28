18ec49a345afb9ab607a05b14eedf503c562b0d2
Josimar Camilo <josimarifmg@gmail.com>
2025-10-27 21:47:55 -0300

    Edit collaborator

---
+++ pages/collaborators/[id]/edit/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { definePageMeta } from "#imports";
+
+useHead({
+	title: "Editar Colaborador",
+	titleTemplate: (titleChunk) => {
+		return titleChunk ? `${titleChunk} | YPet` : "YPet";
+	},
+});
+
+definePageMeta({
+	layout: "default",
+	middleware: ["auth"],
+	name: "collaborators-edit",
+});
+
+defineComponent({
+	name: "PagesCollaboratorsEdit",
+});
+</script>
+
+<template>
+	<TemplatesCollaboratorsEdit />
+</template>
--- stores/collaborators/useEditStore.ts
+++ stores/collaborators/useEditStore.ts
@@ -1,46 +1,123 @@
+import { useForm } from "~/composables/useForm";
+
 export const useEditStore = defineStore("edit", {
 	state: () => {
-		const user = ref({} as IUser);
 		const isLoading = ref(false);
 		const errorMessage = ref("");
 		const successMessage = ref("");
+		const form = useForm([
+			"work_started_at",
+			"observations",
+			"user_role",
+			"user_email",
+			"user_name",
+			"user_password",
+			"user_password_confirmation",
+			"user_status",
+		]);
 		const pathUrl = "/api/collaborators";
 
 		return {
-			user,
 			isLoading,
 			errorMessage,
 			successMessage,
+			form,
 			pathUrl,
 		};
 	},
 	actions: {
-		async update(): Promise<void> {
-			if (this.isLoading) {
-				return;
+		clearForm(): void {
+			for (const key in this.form) {
+				if (Object.prototype.hasOwnProperty.call(this.form, key)) {
+					this.form[key].value = null;
+					this.form[key].errorMessages = [];
+				}
 			}
+			this.errorMessage = "";
+			this.successMessage = "";
+		},
+		setFormField(field: string, value: any): void {
+			this.form[field].value = value;
+			this.setFormError(field, []);
+		},
+		setFormError(field: string, errorMessages: string[]): void {
+			this.form[field].errorMessages = errorMessages;
+		},
+		getFormData(): FormData {
+			const formData: FormData = new FormData();
+			for (const key in this.form) {
+				if (!Object.prototype.hasOwnProperty.call(this.form, key)) continue;
+
+				// Não enviar email no update
+				if (key === "user_email") continue;
+
+				const value = this.form[key].value as string | IOption;
+
+				if (value === null || value === undefined) continue;
+
+				if (typeof value === "object") {
+					if (Object.prototype.hasOwnProperty.call(value, "id")) {
+						formData.set(key, String((value as IOption).id));
+						continue;
+					}
+				}
+				console.log("key", key, "value", value);
+				formData.set(key, String(value));
+			}
+
+			return formData;
+		},
+		handleResponseError(response: IResponse): void {
+			if (response.type !== "error") return;
+
+			this.errorMessage =
+				response.message || "Erro ao processar a solicitação.";
+
+			if (response.status === 401) {
+				this.errorMessage =
+					response.message ||
+					"Não autorizado. Por favor, faça login novamente.";
+			}
+
+			if (response.status === 422) {
+				for (const field in response.errors) {
+					if (Object.prototype.hasOwnProperty.call(response.errors, field)) {
+						this.setFormError(field, response.errors[field]);
+					}
+				}
+			}
+
+			this.isLoading = false;
+		},
+		async update(id: string | number): Promise<void> {
+			if (this.isLoading) return;
+			if (!id) return;
 
 			this.isLoading = true;
 			this.errorMessage = "";
 			this.successMessage = "";
-			await useFetch(`${this.pathUrl}/${this.user.id}`, {
+
+			const formData = this.getFormData();
+
+			console.log("formDataEdit", formData, this.form);
+
+			await useFetch(`${this.pathUrl}/${id}`, {
 				method: "PUT",
-				body: this.user,
+				body: formData,
 				onResponse: ({ response }) => {
-					const result = response._data as IResponse;
-					this.user = result.data || ({} as IUser);
-					this.successMessage = "Atualizado com sucesso!";
+					const responseData = (response._data || {}) as IResponse;
+
+					if (responseData.type === "error") {
+						return this.handleResponseError(responseData);
+					}
+
+					this.successMessage =
+						responseData.message || "Atualizado com sucesso!";
 					this.isLoading = false;
 				},
 				onResponseError: ({ response }) => {
 					this.isLoading = false;
-
-					if (response.status === 422) {
-						this.errorMessage = response._data.message || "Erro de validação.";
-						return;
-					}
-
-					this.errorMessage = response._data.message || "Erro ao atualizar.";
+					this.errorMessage = response._data?.message || "Erro ao atualizar.";
 				},
 			});
 		},
