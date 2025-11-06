16d805baab9a495b571cec8c4130cff17acfecb0
Rafael Ferreira dos Santos <r12rafasantos@gmail.com>
2025-11-06 15:35:12 -0300

    refactor: :recycle: Ajustes no detalhes

--- components/organisms/CastraMobile/Registrations/Details/index.vue
+++ components/organisms/CastraMobile/Registrations/Details/index.vue
@@ -28,62 +28,23 @@ export default defineComponent({
 				? `${data.animal.weight} kg`
 				: "N/A";
 
+			
+			const reproductive = {
+				0: "Secreção vulvar",
+				1: "Secreção prepucial",
+				2: "Testículo ectópico"
+			}
+
 			return [
 				{
 					title: "Sobre o registro",
 					content: [
 						{ label: "ID:", value: data?.id || "N/A" },
-						{ label: "Agendador em:", value: schedulerAt },
 						{ label: "Status:", value: data.status || "N/A", isEnum: true },
 						{ label: "Criado em:", value: createdAt },
 						{ label: "Tutor:", value: data.user?.name || "N/A" },
 					],
 				},
-				{
-					title: "Sobre o animal",
-					content: [
-						{ label: "ID:", value: data?.animal?.id || "N/A" },
-						{
-							label: "Inscrição:",
-							value: data?.animal?.entry_data?.registration_number || "N/A",
-						},
-						{ label: "Nome do animal:", value: data?.animal?.name || "N/A" },
-						{
-							label: "Data do cadastro:",
-							value: data?.animal?.created_at
-								? useDayjs(data.animal.created_at).format("DD/MM/YYYY")
-								: "N/A",
-						},
-						{
-							label: "Tipo de pet: ",
-							value: data?.animal?.species || "N/A",
-							isEnum: true,
-						},
-						{
-							label: "Sexo: ",
-							value: data?.animal?.gender || "N/A",
-							isEnum: true,
-						},
-						{
-							label: "Porte: ",
-							value: data?.animal?.size || "N/A",
-							isEnum: true,
-						},
-						{ label: "Peso:", value: animalWeight },
-						{ label: "Idade: ", value: animalAgeText },
-						{
-							label: "Status:",
-							value: data?.animal?.status || "N/A",
-							isEnum: true,
-						},
-					],
-				},
-			] as AboutType[];
-		});
-
-		const aboutTriage = computed<AboutType[]>(() => {
-			const data = detailsStore.data;
-			return [
 				{
 					title: "Sobre triagem",
 					content: [
@@ -132,7 +93,7 @@ export default defineComponent({
 						},
 						{
 							label: "Avaliação Reprodutiva: ",
-							value: data?.pre_surgery_assessment?.vulvar_discharge,
+							value: reproductive[data?.pre_surgery_assessment?.vulvar_discharge],
 						},
 						{
 							label: "Observações Gerais: ",
@@ -148,8 +109,9 @@ export default defineComponent({
 						},
 					],
 				},
-			];
+			] as AboutType[];
 		});
+
 
 		onMounted(async () => {
 			const id = useRoute().params.id as string;
@@ -160,7 +122,6 @@ export default defineComponent({
 
 		return {
 			abouts,
-			aboutTriage,
 		};
 	},
 });
@@ -170,47 +131,6 @@ export default defineComponent({
 	<div class="wrapper-details">
 		<div
 			v-for="(about, key) in abouts"
-			:key="key"
-			class="wrapper-details__about"
-		>
-			<div class="wrapper-details__about-title">
-				<AtomsTypography
-					type="text-p2"
-					:text="about.title"
-					weight="medium"
-					color="var(--brand-color-dark-blue-700)"
-				/>
-			</div>
-
-			<div class="wrapper-details__about-content">
-				<div
-					v-for="(item, index) in about.content"
-					:key="index"
-					class="wrapper-details__about-content__item"
-				>
-					<AtomsTypography
-						type="text-p4"
-						:text="item.label"
-						weight="medium"
-						color="var(--brand-color-dark-blue-300)"
-					/>
-					<AtomsTypography
-						v-if="!item.isEnum"
-						type="text-p5"
-						:text="(item.value as string) || 'N/A'"
-						weight="regular"
-						color="var(--brand-color-dark-blue-900)"
-					/>
-					<AtomsBadges
-						v-else
-						:text="(item.value as IEnum)?.label || 'N/A'"
-						:custom-color="(item.value as IEnum)?.color || 'light'"
-					/>
-				</div>
-			</div>
-		</div>
-		<div
-			v-for="(about, key) in aboutTriage"
 			:key="key"
 			class="wrapper-details__about"
 		>
